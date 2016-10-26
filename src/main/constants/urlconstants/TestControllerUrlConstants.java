bc9f148de4dd5ab95829484c54a80a81d90c9ba3
michael112 <empty@empty.com>
2016-10-26 17:59:32 +0200

    _

--- src/main/constants/urlconstants/TestControllerUrlConstants.java
+++ src/main/constants/urlconstants/TestControllerUrlConstants.java
@@ -2,6 +2,14 @@ package main.constants.urlconstants;
 
 public class TestControllerUrlConstants {
 
+    public static final String CLASS_URL = GlobalUrlConstants.GLOBAL_API_URL + "/course/{courseID}/test";
 
+    public static final String TEST_LIST = "/list";
+
+    public static final String ADD_TEST  = "";
+
+    public static final String EDIT_TEST = "/{testID}";
+
+    public static final String DELETE_TEST = "{testID}";
 
 }
--- src/main/controllers/TestController.java
+++ src/main/controllers/TestController.java
@@ -1,7 +1,48 @@
 package main.controllers;
 
+import javax.annotation.security.RolesAllowed;
+
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import main.security.coursemembership.annotations.CourseMembershipRequired;
+import main.security.coursemembership.annotations.CourseMembershipType;
+
+import main.constants.urlconstants.TestControllerUrlConstants;
+import main.constants.rolesallowedconstants.RolesAllowedConstants;
+
+import main.json.response.AbstractResponseJson;
+
 public class TestController {
 
+    @RolesAllowed(RolesAllowedConstants.USER)
+    @CourseMembershipRequired
+    @RequestMapping(value = TestControllerUrlConstants.TEST_LIST, method = RequestMethod.GET, produces = "application/json")
+    public ResponseEntity<? extends AbstractResponseJson> getTestList() {
+        throw new org.apache.commons.lang3.NotImplementedException("");
+    }
 
+    @RolesAllowed(RolesAllowedConstants.USER)
+    @CourseMembershipRequired(type = CourseMembershipType.TEACHER)
+    @RequestMapping(value = TestControllerUrlConstants.ADD_TEST, method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
+    public ResponseEntity<? extends AbstractResponseJson> addTest() {
+        throw new org.apache.commons.lang3.NotImplementedException("");
+    }
+
+    @RolesAllowed(RolesAllowedConstants.USER)
+    @CourseMembershipRequired(type = CourseMembershipType.TEACHER)
+    @RequestMapping(value = TestControllerUrlConstants.EDIT_TEST, method = RequestMethod.PUT, consumes = "application/json", produces = "application/json")
+    public ResponseEntity<? extends AbstractResponseJson> editTest() {
+        throw new org.apache.commons.lang3.NotImplementedException("");
+    }
+
+    @RolesAllowed(RolesAllowedConstants.USER)
+    @CourseMembershipRequired(type = CourseMembershipType.TEACHER)
+    @RequestMapping(value = TestControllerUrlConstants.DELETE_TEST, method = RequestMethod.DELETE, consumes = "application/json", produces = "application/json")
+    public ResponseEntity<? extends AbstractResponseJson> deleteTest() {
+        throw new org.apache.commons.lang3.NotImplementedException("");
+    }
 
 }
