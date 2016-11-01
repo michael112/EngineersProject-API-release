7dbba5f463a5dde2b634622ef045c1e1580508cf
michael112 <empty@empty.com>
2016-11-01 14:53:25 +0100

    _

--- src/test/runtime/tests/controllers/MessageControllerTest.java
+++ src/test/runtime/tests/controllers/MessageControllerTest.java
@@ -24,6 +24,7 @@ import main.util.currentUser.CurrentUserService;
 import main.util.labels.LabelProvider;
 import main.util.domain.DomainURIProvider;
 import main.util.coursemembership.validator.CourseMembershipValidator;
+import main.util.mail.MailSender;
 
 import main.constants.urlconstants.MessageControllerUrlConstants;
 
@@ -32,18 +33,20 @@ import main.service.file.FileUploadService;
 import main.service.crud.course.course.CourseCrudService;
 import main.service.crud.course.file.FileCrudService;
 import main.service.crud.course.message.MessageCrudService;
+import main.service.crud.user.user.UserCrudService;
 
 import main.model.user.User;
 import main.model.course.Course;
 import main.model.course.File;
 import main.model.course.Message;
 
+import main.json.course.message.NewMessageJson;
+
 import test.runtime.environment.TestEnvironment;
 import test.runtime.environment.TestEnvironmentBuilder;
 
 import static org.hamcrest.Matchers.*;
 import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import static org.mockito.Mockito.reset;
@@ -67,6 +70,8 @@ public class MessageControllerTest extends AbstractControllerTest {
     private CourseMembershipValidator courseMembershipValidatorMock;
     @Autowired
     private LocaleResolver localeResolverMock;
+    @Autowired
+    private MailSender mailSenderMock;
 
     @Autowired
     private FileCrudService fileCrudServiceMock;
@@ -74,13 +79,15 @@ public class MessageControllerTest extends AbstractControllerTest {
     private CourseCrudService courseCrudServiceMock;
     @Autowired
     private MessageCrudService messageCrudServiceMock;
+    @Autowired
+    private UserCrudService userCrudServiceMock;
 
     private String testedClassURI;
 
     private TestEnvironment testEnvironment;
 
     public void setMockito() {
-        reset(labelProviderMock, domainURIProviderMock, currentUserServiceMock, fileUploadServiceMock);
+        reset(labelProviderMock, domainURIProviderMock, currentUserServiceMock, fileUploadServiceMock, fileCrudServiceMock, courseCrudServiceMock, userCrudServiceMock);
         this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
     }
 
@@ -100,21 +107,23 @@ public class MessageControllerTest extends AbstractControllerTest {
         User sampleSender = this.testEnvironment.getUsers().get(2);
         User sampleReceiver = this.testEnvironment.getUsers().get(0);
         MockMultipartFile fileToSend = new MockMultipartFile("attachement", "filename.txt", "text/plain", "sample text".getBytes());
-        File sampleAttachement = new File(fileToSend.getName(), Calendar.getInstance().getTime(), "", sampleSender);
+        MockMultipartFile newHomeworkJson = new MockMultipartFile("json", "", "application/json", objectToJsonBytes(new NewMessageJson("sample message title", "sample message content")));
+        // File sampleAttachement = new File(fileToSend.getName(), Calendar.getInstance().getTime(), "", sampleSender);
 
+        when(labelProviderMock.getLabel(Mockito.any(String.class))).thenReturn(returnMessage);
         when(currentUserServiceMock.getCurrentUser()).thenReturn(sampleSender);
-        when(fileUploadServiceMock.uploadFile(Mockito.any(MultipartFile.class), Mockito.any(User.class))).thenReturn(sampleAttachement);
-        doNothing().when(fileCrudServiceMock).saveFile(Mockito.any(File.class));
-        doNothing().when(courseCrudServiceMock).updateCourse(Mockito.any(Course.class));
+        when(userCrudServiceMock.findUserByID(sampleReceiver.getId())).thenReturn(sampleReceiver);
+        doNothing().when(mailSenderMock).sendMail(Mockito.any(String.class), Mockito.any(String.class), Mockito.any(String.class));
+
         when(courseMembershipValidatorMock.isStudent(Mockito.any(User.class), Mockito.any(Course.class))).thenReturn(true);
         when(courseMembershipValidatorMock.isTeacher(Mockito.any(User.class), Mockito.any(Course.class))).thenReturn(false);
         when(courseMembershipValidatorMock.isStudentOrTeacher(Mockito.any(User.class), Mockito.any(Course.class))).thenReturn(true);
-        when(labelProviderMock.getLabel(Mockito.any(String.class))).thenReturn(returnMessage);
 
-        String URL = this.testedClassURI + MessageControllerUrlConstants.SEND_MESSAGE + "?type=user&id=" + sampleReceiver.getId();
+        String URL = this.testedClassURI + '/' + MessageControllerUrlConstants.SEND_MESSAGE + "?type=user&id=" + sampleReceiver.getId();
 
         this.mockMvc.perform(MockMvcRequestBuilders.fileUpload(URL)
             .file(fileToSend)
+            .file(newHomeworkJson)
             )
             .andExpect(status().isOk())
             .andExpect(content().contentType("application/json;charset=utf-8"))
@@ -124,28 +133,30 @@ public class MessageControllerTest extends AbstractControllerTest {
 
     @Test
     public void testSendGroupMessage() throws Exception {
-        Assert.fail();
-
         String returnMessage = "";
 
         User sampleSender = this.testEnvironment.getUsers().get(2);
         Course sampleCourse = this.testEnvironment.getCourses().get(0);
         MockMultipartFile fileToSend = new MockMultipartFile("attachement", "filename.txt", "text/plain", "sample text".getBytes());
+        MockMultipartFile newHomeworkJson = new MockMultipartFile("json", "", "application/json", objectToJsonBytes(new NewMessageJson("sample message title", "sample message content", true)));
         File sampleAttachement = new File(fileToSend.getName(), Calendar.getInstance().getTime(), "", sampleSender);
 
+        when(labelProviderMock.getLabel(Mockito.any(String.class))).thenReturn(returnMessage);
         when(currentUserServiceMock.getCurrentUser()).thenReturn(sampleSender);
+        when(courseCrudServiceMock.findCourseByID(Mockito.any(String.class))).thenReturn(sampleCourse);
+        doNothing().when(mailSenderMock).sendMail(Mockito.any(String.class), Mockito.any(String.class), Mockito.any(String.class));
         when(fileUploadServiceMock.uploadFile(Mockito.any(MultipartFile.class), Mockito.any(User.class))).thenReturn(sampleAttachement);
-        doNothing().when(fileCrudServiceMock).saveFile(Mockito.any(File.class));
-        doNothing().when(courseCrudServiceMock).updateCourse(Mockito.any(Course.class));
+        doNothing().when(messageCrudServiceMock).saveMessage(Mockito.any(Message.class));
+
         when(courseMembershipValidatorMock.isStudent(Mockito.any(User.class), Mockito.any(Course.class))).thenReturn(true);
         when(courseMembershipValidatorMock.isTeacher(Mockito.any(User.class), Mockito.any(Course.class))).thenReturn(false);
         when(courseMembershipValidatorMock.isStudentOrTeacher(Mockito.any(User.class), Mockito.any(Course.class))).thenReturn(true);
-        when(labelProviderMock.getLabel(Mockito.any(String.class))).thenReturn(returnMessage);
 
-        String URL = this.testedClassURI + MessageControllerUrlConstants.SEND_MESSAGE + "?type=group&id=" + sampleCourse.getId();
+        String URL = this.testedClassURI + '/' + MessageControllerUrlConstants.SEND_MESSAGE + "?type=group&id=" + sampleCourse.getId();
 
         this.mockMvc.perform(MockMvcRequestBuilders.fileUpload(URL)
             .file(fileToSend)
+            .file(newHomeworkJson)
             )
             .andExpect(status().isOk())
             .andExpect(content().contentType("application/json;charset=utf-8"))
