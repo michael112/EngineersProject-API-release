88d376db8539daf8a6bc63f3c16c5c75728cb6f7
michael112 <empty@empty.com>
2016-11-01 08:53:18 +0100

    _

---
+++ src/main/controllers/MessageController.java
@@ -0,0 +1,14 @@
+package main.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import main.constants.urlconstants.MessageControllerUrlConstants;
+
+@RequestMapping(value = MessageControllerUrlConstants.CLASS_URL)
+@RestController
+public class MessageController {
+
+
+
+}
