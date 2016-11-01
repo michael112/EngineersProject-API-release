ac106bbe4c6a291ece236559ffc7d22f21583a79
michael112 <empty@empty.com>
2016-11-01 09:06:33 +0100

    _

--- src/main/controllers/MessageController.java
+++ src/main/controllers/MessageController.java
@@ -3,7 +3,6 @@ package main.controllers;
 import org.springframework.http.ResponseEntity;
 
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,12 +16,12 @@ import main.json.response.AbstractResponseJson;
 public class MessageController {
 
     @RequestMapping(value = MessageControllerUrlConstants.SEND_MESSAGE, method = RequestMethod.POST, produces = "application/json", params = "type=user")
-    public ResponseEntity<? extends AbstractResponseJson> sendUserMessage(@PathVariable("id") String userID, @RequestParam("type") String type) {
+    public ResponseEntity<? extends AbstractResponseJson> sendUserMessage(@PathVariable("id") String userID) {
         throw new org.apache.commons.lang3.NotImplementedException("");
     }
 
     @RequestMapping(value = MessageControllerUrlConstants.SEND_MESSAGE, method = RequestMethod.POST, produces = "application/json", params = "type=group")
-    public ResponseEntity<? extends AbstractResponseJson> sendGroupMessage(@PathVariable("id") String groupID, @RequestParam("type") String type) {
+    public ResponseEntity<? extends AbstractResponseJson> sendGroupMessage(@PathVariable("id") String groupID) {
         throw new org.apache.commons.lang3.NotImplementedException("");
     }
 
