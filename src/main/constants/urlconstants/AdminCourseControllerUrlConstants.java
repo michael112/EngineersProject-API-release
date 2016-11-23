3282a3a1a4fdd4ccdbc96ee8a62e097eb0af0e26
michalch1992@gmail.com <michalch1992@gmail.com>
2016-11-23 10:57:50 +0100

    _

--- src/main/constants/urlconstants/AdminCourseControllerUrlConstants.java
+++ src/main/constants/urlconstants/AdminCourseControllerUrlConstants.java
@@ -2,6 +2,36 @@ package main.constants.urlconstants;
 
 public class AdminCourseControllerUrlConstants {
 
+    public static final String CLASS_URL = GlobalUrlConstants.GLOBAL_API_URL + "/admin/course";
 
+    public static final String COURSE_INFO = "/{courseID}";
+
+    public static final String AVAILABLE_CREATING_COURSE_DATA = "";
+
+    public static final String ADD_COURSE = "";
+
+    public static final String EDIT_COURSE = "/{courseID}";
+
+    public static final String EDIT_COURSE_LANGUAGE = "/{courseID}/language";
+
+    public static final String EDIT_COURSE_TYPE = "/{courseID}/coursetype";
+
+    public static final String EDIT_COURSE_LEVEL = "/{courseID}/level";
+
+    public static final String EDIT_COURSE_ACTIVITY = "/{courseID}/activity";
+
+    public static final String EDIT_COURSE_DAYS = "/{courseID}/days";
+
+    public static final String EDIT_COURSE_TEACHER = "/{courseID}/teacher";
+
+    public static final String EDIT_COURSE_ADD_TEACHER = "/{courseID}/teacher/add/{teacherID}";
+
+    public static final String EDIT_COURSE_REMOVE_TEACHER = "/{courseID}/teacher/remove/{teacherID}";
+
+    public static final String EDIT_COURSE_MAX_STUDENTS = "/{courseID}/max/students";
+
+    public static final String EDIT_COURSE_PRICE = "/{courseID}/price";
+
+    public static final String REMOVE_COURSE = "/{courseID}";
 
 }
