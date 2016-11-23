d88beb1ed6b1aafd5ef8cceaa28efde31b041958
michalch1992@gmail.com <michalch1992@gmail.com>
2016-11-23 11:07:55 +0100

    _

--- src/main/service/controller/admin/course/AdminCourseServiceImpl.java
+++ src/main/service/controller/admin/course/AdminCourseServiceImpl.java
@@ -1,9 +1,12 @@
 package main.service.controller.admin.course;
 
+import org.springframework.stereotype.Service;
+
 import main.util.locale.LocaleCodeProvider;
 
 import main.service.controller.AbstractService;
 
+@Service("adminCourseService")
 public class AdminCourseServiceImpl extends AbstractService implements AdminCourseService {
 
     public AdminCourseServiceImpl(LocaleCodeProvider localeCodeProvider) {
