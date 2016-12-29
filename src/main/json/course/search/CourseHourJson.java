e56c8247026e2448bef17a4b1a075bbd1e26fdd6
michalch1992@gmail.com <michalch1992@gmail.com>
2016-12-29 16:38:47 +0100

    _

--- src/main/json/course/search/CourseHourJson.java
+++ src/main/json/course/search/CourseHourJson.java
@@ -1,5 +1,8 @@
 package main.json.course.search;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Max;
+
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,10 +10,14 @@ import lombok.Setter;
 @EqualsAndHashCode
 public class CourseHourJson {
 
+    @Min(value = 0, message = "course.search.hour")
+    @Max(value = 24, message = "course.search.hour")
     @Getter
     @Setter
     private int hour;
 
+    @Min(value = 0, message = "course.search.minute")
+    @Max(value = 59, message = "course.search.minute")
     @Getter
     @Setter
     private int minute;
