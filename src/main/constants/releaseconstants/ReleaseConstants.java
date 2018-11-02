720bd23ededae41bb30a9b232d028fc893d04f8e
michael112 <empty@empty.com>
2018-11-02 18:56:30 +0100

    added release constants values

---
+++ src/main/constants/releaseconstants/ReleaseConstants.java
@@ -0,0 +1,9 @@
+package main.constants.releaseconstants;
+
+public class ReleaseConstants {
+
+    public static final String DEVELOPMENT_RELEASE = "DEVELOPMENT";
+    public static final String DEMO_RELEASE = "DEMONSTRATION";
+    public static final String PRODUCTION_RELEASE = "PRODUCTION";
+
+}
